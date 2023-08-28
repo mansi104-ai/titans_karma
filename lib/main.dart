863fc1ba5b1a9bb56bfea778f6dd2bcbbf59b20e
mansi104-ai <118623172+mansi104-ai@users.noreply.github.com>
2023-08-28 15:11:25 +0530

    final changes made

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:pec_chat/providers/user_provider.dart';
 import 'package:pec_chat/responsive/mobile_screen_layout.dart';
 import 'package:pec_chat/responsive/responsive_layout.dart';
 import 'package:pec_chat/responsive/web_screen_layout.dart';
 import 'package:pec_chat/screens/login_screen.dart';
 import 'package:pec_chat/utils/colors.dart';
-import 'package:provider/provider.dart';
+
 
 
 void main() async {
@@ -20,7 +19,6 @@ void main() async {
       options: const FirebaseOptions(
           apiKey: "AIzaSyCv-6HKeFZh1X4IfoQHtUwMzALGsa5OcxE",
           appId: "1:11962779834:android:a7824fa052aa445cae1a83",
-          messagingSenderId: "11962779834",
           projectId: "pec-chat",
           storageBucket: 'pec-chat.com'),
     );
